1584f789995b0ffc3bf1550ad82e005a88ba85f8
Rodrigo Hernández Góngora <rgongora@grupodel.mx>
2019-10-02 23:21:09 -0500

    Create area.java
    Creado la clase Area

---
+++ area.java
@@ -0,0 +1,35 @@
+public class Area {
+    private String areaName;
+    private String areaId;
+
+    Area(String areaName, String areaId){
+    	this.areaName=areaName;
+    	this.areaId=areaId;
+    }
+
+    public void setAreaName(String areaName){
+    	this.areaName=areaName;
+    }
+
+    public void setAreaId(String areaId){
+    	this.areaId=areaId;
+    }
+
+    public String getAreaName(){
+    	return areaName;
+    }
+
+    public String getAreaId(){
+    	return areaId;
+    }
+
+    public void addRoom(Room newRoom){
+    	room.put(newRoom.getRoomId(), newRoom)
+    }
+
+    public void removeRoom(String roomID){
+    	if(room.containsKey(roomID)){
+    		room.remove(roomID);
+    	}
+    }
+}
