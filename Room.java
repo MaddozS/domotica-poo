20b249ae13898cef7984469f65e6ad8646c9bc86
fadedphoenix7 <43895609+fadedphoenix7@users.noreply.github.com>
2019-10-02 22:51:19 -0500

    Add files via upload

--- Room.java
+++ Room.java
@@ -1,15 +1,16 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Room{
 
-	private int devices[];
+	private Map <String,Integer> devices;
 	private String name;
 	private int id;
 	private static int numRoomes = 1;
 	private int numDevices;
 
 	public Room(String name){
-		devices = new int[100];
+		devices = new HashMap<>();
 		this.numDevices = 0;
 		this.name = name;
 		this.id = numRoomes;
@@ -17,17 +18,11 @@ public class Room{
 	}
 
 	public void addDevice(Dispositivo device){
-		Arrays.sort(devices);
-		if(Arrays.binarySearch(devices,device.getId()) <= 0){
-			System.out.println(device.getId());
-			this.devices[numDevices] = device.getId(); 
-			this.numDevices++;
-		}
+		devices.putIfAbsent(device.getName(),device.getId());
 	}
 
-	public int[] getDisList(){
-		int[] newDevices = Arrays.copyOfRange(this.devices,100 - numDevices, 100);
-		return newDevices;
+	public Map getDeviceList(){
+		return devices;
 	}
 
 	public int getId(){
