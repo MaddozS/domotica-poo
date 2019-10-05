6a9f163016c4234090501f8f868f2b3798c28ca9
Rodrigo Hernández Góngora <rgongora@grupodel.mx>
2019-10-04 22:03:14 -0500

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,34 +1,36 @@
-import java.util.HashMap;
-
-public class Area{
+public class Area {
     private String areaName;
-    private static int areaId;
-    public HashMap<Integer,Room> rooms = new HashMap<>();
+    private String areaId;
+    public Map<String, Room> rooms=new HashMap<>();
 
-    public Area(String areaName){
-        areaId++;
+    Area(String areaName, String areaId){
     	this.areaName=areaName;
+    	this.areaId=areaId;
     }
 
     public void setAreaName(String areaName){
     	this.areaName=areaName;
     }
 
+    public void setAreaId(String areaId){
+    	this.areaId=areaId;
+    }
+
     public String getAreaName(){
     	return areaName;
     }
 
-    public int getId(){
+    public String getAreaId(){
     	return areaId;
     }
 
     public void addRoom(Room newRoom){
-    	rooms.put(newRoom.getId(), newRoom);
+    	room.put(newRoom.getRoomId(), newRoom)
     }
 
-    public void removeRoom(int roomID){
-    	if(rooms.get(roomID) != null){
-    		rooms.remove(roomID);
+    public void removeRoom(String roomID){
+    	if(room.containsKey(roomID)){
+    		room.remove(roomID);
     	}
     }
 }
